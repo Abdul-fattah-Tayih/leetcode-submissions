3338212b9025fc8b4155a064ce48321e861e1fcf
Abdul-fattah Tayih <abedalfatah.tayeh@yahoo.com>
2022-11-28 16:45:49 +0300

    Solve 1 and 242

---
+++ solutions/two_sum.py
@@ -0,0 +1,40 @@
+from typing import List
+
+class TwoSum:
+    """
+        1. Two Sum
+
+        Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+        You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+        You can return the answer in any order.
+    """
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        """
+            O(n)
+
+            We use a hash map to store all values as the keys and their indices as the values
+
+            Then we iterate over the list and in each iteration we check the if the difference between the current
+
+            number and the target exists in the hash map, making sure not to use the same element twice
+
+            https://leetcode.com/submissions/detail/851187347/
+        """
+        number_indices = {value: index for index, value in enumerate(nums)}
+        
+        for index, num in enumerate(nums):
+            required_number = target - num
+            
+            if required_number in number_indices and index != number_indices[required_number]:
+                return [index, number_indices[required_number]]
+            
+        return []
+
+if __name__ == '__main__':
+    obj = TwoSum()
+
+    assert obj.twoSum([2,7,11,15], 9) == [0, 1]
+    assert obj.twoSum([3,2,4], 6) == [1, 2]
+    assert obj.twoSum([3,3], 6) == [0, 1]
---
+++ solutions/valid_anagram.py
@@ -0,0 +1,35 @@
+class ValidAnagram:
+    """
+        242. Valid Anagram
+
+        Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+
+        An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, 
+        
+        typically using all the original letters exactly once.
+    """
+    def isAnagram(self, s: str, t: str) -> bool:
+        """
+            O(n)
+
+            We create a frequency dict for both strings, if both dicts are equal
+
+            then the 2 strings are anagrams, otherwise they're not
+
+            We can even do the equality check of the dicts manually, 
+            
+            first we check if they have equal amount of keys and if not then they're not the same, 
+            
+            Then we need to iterate over one dict and check if all the values of each character are the same
+
+            https://leetcode.com/submissions/detail/851190369/
+        """
+        s_dict = {}
+        for char in s:
+            s_dict[char] = s_dict.get(char, 0) + 1
+            
+        t_dict = {}
+        for char in t:
+            t_dict[char] = t_dict.get(char, 0) + 1
+            
+        return s_dict == t_dict
