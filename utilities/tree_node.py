28c48ae78692879b91a7af5d2eea044c08ddf5b3
Abdul-fattah Tayih <abedalfatah.tayeh@yahoo.com>
2023-01-05 00:23:39 +0300

    add tree_node file

--- utilities/tree_node.py
+++ utilities/tree_node.py
@@ -1,5 +1,8 @@
+from typing import Optional
+
+
 class TreeNode:
-    def __init__(self, val=0, left=None, right=None):
+    def __init__(self, val: int = 0, left: Optional['TreeNode'] = None, right: Optional['TreeNode'] = None):
         self.val = val
         self.left = left
         self.right = right
