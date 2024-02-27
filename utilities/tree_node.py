cfe32e8d3ff444f72c14311058f1f0f31c61ac20
Abdul-fattah-Tayih <abdul-fattah-tayih@gmail.com>
2024-02-27 19:15:03 +0300

    add generate_from_list utility function to TreeNode class

--- utilities/tree_node.py
+++ utilities/tree_node.py
@@ -1,4 +1,5 @@
-from typing import Optional
+from collections import deque
+from typing import List, Optional
 
 
 class TreeNode:
@@ -8,4 +9,38 @@ class TreeNode:
         self.right = right
 
     def __repr__(self) -> str:
-        return f'{getattr(self.left, "data") or "None"} <- {self.data} -> {getattr(self.right, "data") or "None"}'
+        return f'{getattr(self.left, "data") or "None"} <- {self.val} -> {getattr(self.right, "data") or "None"}'
+    
+    @classmethod
+    def generate_from_list(cls, list: List[Optional[int]]) -> Optional["TreeNode"]:
+        """
+            Generates a tree object from serialized binary tree format
+
+            https://support.leetcode.com/hc/en-us/articles/360011883654-What-does-1-null-2-3-mean-in-binary-tree-representation
+        """
+        if not List:
+            return None
+        
+        if list[0] is None:
+            return None
+        
+        root = TreeNode(list[0])
+        current = root
+
+        i = 1
+        queue = deque([current])
+
+        for i in range(1, len(list), 2):
+            parent = queue.popleft()
+
+            if list[i] is not None:
+                left = TreeNode(list[i])
+                parent.left = left
+                queue.append(left)
+
+            if i + 1 < len(list) and list[i + 1] is not None:
+                right = TreeNode(list[i + 1])
+                parent.right = right
+                queue.append(right)
+
+        return root
